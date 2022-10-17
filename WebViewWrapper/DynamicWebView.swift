70ef5c999ceb7f5326d4a17412540b675b45d1c0
Dean Chang <dean.chang@freestar.io>
2022-10-17 12:57:35 -0700

    Remove deprecated UIWebView code and center views.

--- WebViewWrapper/DynamicWebView.swift
+++ WebViewWrapper/DynamicWebView.swift
@@ -17,20 +17,11 @@ import WebKit
 final public class DynamicWebView: UIView {
     
     public weak var delegate: DynamicWebViewDelegate?
-    
-    var useLegacyWebView: Bool?
-    
-    fileprivate lazy var uiWebView: UIWebView  = {
-        var webView = UIWebView(frame: self.frame)
-        webView.delegate = self
-        webView.backgroundColor = UIColor.gray
-//        webView.scrollView.isScrollEnabled = false        
-        return webView
-    }()
-    
+        
     @available(iOS 8.0, *)
     fileprivate lazy var wkWebView: WKWebView  = {
-        var webView = WKWebView(frame: self.frame)
+        var webView = WKWebView(frame: CGRect(x: 0, y: 0, width: 300, height: 250))
+        webView.backgroundColor = UIColor.white
         webView.navigationDelegate = self
         webView.backgroundColor = UIColor.white
 //        webView.scrollView.isScrollEnabled = false
@@ -40,92 +31,47 @@ final public class DynamicWebView: UIView {
     var webBackgroundColor: UIColor? {
         didSet{
             if let webBackgroundColor = webBackgroundColor {
-                if useLegacyWebView! {
-                    self.uiWebView.backgroundColor = webBackgroundColor
-                    self.uiWebView.scrollView.backgroundColor = webBackgroundColor
-                } else {
-                    self.wkWebView.backgroundColor = webBackgroundColor
-                    self.wkWebView.scrollView.backgroundColor = webBackgroundColor
-                }
+                self.wkWebView.backgroundColor = webBackgroundColor
+                self.wkWebView.scrollView.backgroundColor = webBackgroundColor
             }
         }
     }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        useLegacyWebView = false
     }
     
     required public init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        useLegacyWebView = false
+        super.init(coder: aDecoder)        
     }
     
-    fileprivate func setupView() {
-        if useLegacyWebView! {
-            self.addSubview(self.uiWebView)
-            self.uiWebView.translatesAutoresizingMaskIntoConstraints = false
-            let horizontalConstraint = NSLayoutConstraint(item: self.uiWebView, attribute: NSLayoutConstraint.Attribute.top, relatedBy: NSLayoutConstraint.Relation.equal, toItem: self, attribute: NSLayoutConstraint.Attribute.top, multiplier: 1, constant: 0)
-            let verticalConstraint = NSLayoutConstraint(item: self.uiWebView, attribute: NSLayoutConstraint.Attribute.bottom, relatedBy: NSLayoutConstraint.Relation.equal, toItem: self, attribute: NSLayoutConstraint.Attribute.bottom, multiplier: 1, constant: 0)
-            let leftConstraint = NSLayoutConstraint(item: self.uiWebView, attribute: NSLayoutConstraint.Attribute.left, relatedBy: NSLayoutConstraint.Relation.equal, toItem: self, attribute: NSLayoutConstraint.Attribute.left, multiplier: 1, constant: 0)
-            let rightConstraint = NSLayoutConstraint(item: self.uiWebView, attribute: NSLayoutConstraint.Attribute.right, relatedBy: NSLayoutConstraint.Relation.equal, toItem: self, attribute: NSLayoutConstraint.Attribute.right, multiplier: 1, constant: 0)
-            self.addConstraints([horizontalConstraint, verticalConstraint, leftConstraint, rightConstraint])
-        } else {
-            self.addSubview(self.wkWebView)
-            self.wkWebView.translatesAutoresizingMaskIntoConstraints = false
-            let horizontalConstraint = NSLayoutConstraint(item: self.wkWebView, attribute: NSLayoutConstraint.Attribute.top, relatedBy: NSLayoutConstraint.Relation.equal, toItem: self, attribute: NSLayoutConstraint.Attribute.top, multiplier: 1, constant: 0)
-            let verticalConstraint = NSLayoutConstraint(item: self.wkWebView, attribute: NSLayoutConstraint.Attribute.bottom, relatedBy: NSLayoutConstraint.Relation.equal, toItem: self, attribute: NSLayoutConstraint.Attribute.bottom, multiplier: 1, constant: 0)
-            let leftConstraint = NSLayoutConstraint(item: self.wkWebView, attribute: NSLayoutConstraint.Attribute.left, relatedBy: NSLayoutConstraint.Relation.equal, toItem: self, attribute: NSLayoutConstraint.Attribute.left, multiplier: 1, constant: 0)
-            let rightConstraint = NSLayoutConstraint(item: self.wkWebView, attribute: NSLayoutConstraint.Attribute.right, relatedBy: NSLayoutConstraint.Relation.equal, toItem: self, attribute: NSLayoutConstraint.Attribute.right, multiplier: 1, constant: 0)
-            self.addConstraints([horizontalConstraint, verticalConstraint, leftConstraint, rightConstraint])
-            
-        }
+    public func setupView() {
+        self.addSubview(self.wkWebView)
+        self.wkWebView.translatesAutoresizingMaskIntoConstraints = false
+        self.wkWebView.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        self.wkWebView.heightAnchor.constraint(equalToConstant: 250).isActive = true
+        self.wkWebView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        self.wkWebView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
     }
     
     public func loadHTML(_ string: String) {
-        if useLegacyWebView! {
-            self.uiWebView.loadHTMLString(string, baseURL: nil)
-        } else {
-            self.wkWebView.loadHTMLString(string, baseURL: nil)
-        }
+        self.wkWebView.loadHTMLString(string, baseURL: nil)
     }
     
     public func loadRequest(_ urlRequest: URLRequest) {
         self.setupView()
-        if useLegacyWebView! {
-            self.uiWebView.loadRequest(urlRequest)
-        } else {
-            self.wkWebView.load(urlRequest)
-        }
+        self.wkWebView.load(urlRequest)
     }
     
     public func evaluateJS(script: String, result: @escaping (String) -> Void) {
-        if useLegacyWebView! {
-            result(self.uiWebView.stringByEvaluatingJavaScript(from: script) ?? "")
-        } else {
-            self.wkWebView.evaluateJavaScript(script) { (response, error) in
-                if let error = error {
-                    print(error)
-                } else {
-                    if let response  = response {
-                        print(response)
-                    }
+        self.wkWebView.evaluateJavaScript(script) { (response, error) in
+            if let error = error {
+                print(error)
+            } else {
+                if let response  = response {
+                    print(response)
                 }
             }
-        }
-    }
-}
-
-extension DynamicWebView: UIWebViewDelegate {
-    public func webViewDidFinishLoad(_ webView: UIWebView) {
-        if let delegate = self.delegate {
-            delegate.webViewDidFinishedLoading(view: self)
-        }
-    }
-    
-    public func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
-        if let delegate = self.delegate {
-            delegate.webViewDidFailToLoad(view: self, error: error)
         }
     }
 }
@@ -136,6 +82,12 @@ extension DynamicWebView: WKNavigationDelegate {
         if let delegate = self.delegate {
             delegate.webViewDidFinishedLoading(view: self)
         }
+        
+        
+        webView.evaluateJavaScript("document.documentElement.outerHTML.toString()",
+                                   completionHandler: { (html: Any?, error: Error?) in
+            print(html!)
+        })
     }
     
     public func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
--- WebViewWrapper/ViewController.swift
+++ WebViewWrapper/ViewController.swift
@@ -12,23 +12,53 @@ class ViewController: UIViewController, DynamicWebViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.view.addSubview(webView)
-        
-        //        webView.loadHTML("")
-        webView.loadRequest(URLRequest(url: URL(string: "https://google.com")!))
-
+        self.webView.translatesAutoresizingMaskIntoConstraints = false
+        self.webView.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        self.webView.heightAnchor.constraint(equalToConstant: 250).isActive = true
+        self.webView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        self.webView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
     }
     
-    override func viewDidAppear(_ animated: Bool) {
+    override func viewWillAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
+        webView.setupView()
+        
+        loadWebView()
+        delay(15.0) {
+            self.loadWebView()
+        }
+        delay(30.0) {
+            self.loadWebView()
+        }
+        delay(45.0) {
+            self.loadWebView()
+        }
+        delay(60.0) {
+            self.loadWebView()
+        }
+        delay(75.0) {
+            self.loadWebView()
+        }
+    }
+    
+    func delay(_ delay:Double, closure:@escaping ()->()) {
+        let when = DispatchTime.now() + delay
+        DispatchQueue.main.asyncAfter(deadline: when, execute: closure)
+    }
+    
+    
+    func loadWebView() {
+        webView.loadHTML("<div align=\"center\" id=\"nextshark_mrec_smartnews\">\n        <script data-cfasync=\"false\" type=\"text/javascript\">\n            var freestar = freestar || {};\n            freestar.queue = freestar.queue || [];\n            freestar.config = freestar.config || {};\n            freestar.config.enabled_slots = [];\n            freestar.initCallback = function () {\n                (freestar.config.enabled_slots.length === 0) ? freestar.initCallbackCalled = false: freestar\n                    .newAdSlots(freestar.config.enabled_slots)\n            }\n            freestar.config.disabledProducts = {\n                stickyFooter: true,\n                googleInterstitial: true\n            }\n            freestar.queue.push(function () {\n                googletag.pubads().set('page_url', 'https://www.nextshark.com/');\n                freestar.newAdSlots({\n                    placementName: \"nextshark_mrec_smartnews\",\n                    slotId: \"nextshark_mrec_smartnews\"\n                });\n            });\n        </script>\n        <script src=\"https://a.pub.network/nextshark-com/pubfig.min.js\" async></script>\n    </div>")
+        
+//                webView.loadRequest(URLRequest(url: URL(string: "https://google.com")!))
     }
     
     fileprivate lazy var webView: DynamicWebView  = {
-        var webView = DynamicWebView(frame: view.frame)
-        webView.delegate = self
-        webView.useLegacyWebView = true // use UIWebView
+        var webView = DynamicWebView(frame: CGRect(x: 0, y: 0, width: 300, height: 250))
+        webView.backgroundColor = UIColor.cyan
+        webView.delegate = self        
         return webView
     }()
     
